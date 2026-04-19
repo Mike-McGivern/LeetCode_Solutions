c8a7598d539fbd2b6308bf6dfbcf534c6da7684c
Michael P McGivern II <mpm118@pitt.edu>
2026-04-19 09:28:46 -0400

    Implement maxDistance function to find max distance

---
+++ 1855_Maximum_Distance_Between_Pair_of_Values.c
@@ -0,0 +1,16 @@
+int maxDistance(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    int i = 0, j = 0;
+    int best = 0;
+
+    while(i < nums1Size && j < nums2Size) {
+        if(nums1[i] <= nums2[j]) {
+            if(j - i > best)
+                best = j - i;
+            j++;
+        } else {
+            i++;
+        }
+    }
+
+    return best;
+}
